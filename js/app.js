e9ea2c4404341ad0055031f2e25f85c00bc32ac1
Asadsheblu <shebluasad@gmail.com>
2022-02-28 21:52:51 +0600

    functionality-add

--- js/app.js
+++ js/app.js
@@ -2,11 +2,21 @@ const phoneSearch=()=>{
     //search value access
    const search=document.getElementById('search-box')
   const searchText=search.value ;
-
-  //api access
+  const errorMsg= document.getElementById('errorMsg')
+  errorMsg.innerText=''
+//error handeling
+if(searchText==''){
+   
+   errorMsg.innerHTML='Please Search Any Kinds Of Phone Name'
+   return
+}
+else{
+    //api access
   fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
   .then(res=>res.json())
   .then(data=>displayData(data.data))
+}
+  
   
 }
 //function for display data
@@ -15,20 +25,71 @@ const displayData=(datas)=>{
     // console.log(datas.brand);
 
     const searchResult=document.getElementById('search-result')
+    // const errorMsg= document.getElementById('errorMsg')
+    
+    //error handling for wrong search
+    // if(datas==null){
+    //     alert('error')
+    // }
+    
     datas.forEach(data=>{
         // console.log(data.phone_name);
         const div=document.createElement('div')
         div.classList.add('col')
         div.innerHTML=`
-        <div class="card">
-        <img src="${data.image}" class="card-img-top" alt="...">
+        <div class="card m-3">
+        <img  class='img-fluid w-75' src="${data.image}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h4 class="card-title">${data.phone_name}</h4>
+          <h5 class="card-title">${data.brand}</h5>
+          <button class="btn btn-success" onclick="details('${data.slug}')">Details</button>
+          
         </div>
       </div>
     </div>
         `
         searchResult.appendChild(div)
     })
+   
+}
+
+const details=(id)=>{
+    fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    .then(res=>res.json())
+    .then(data=>displayDetails(data.data))
+}
+
+const displayDetails=(details)=>{
+// console.log(details);
+    const phoneDetails=document.getElementById('details')
+    phoneDetails.innerHTML=`
+    <div class="col-md-12 ">
+    <img class="img-fluid" src="${details.image}" alt="" srcset="">
+  <div class="card-body">
+    <h5 class="card-title">${details.name}</h5>
+    <h6>${details.releaseDate}</h6>
+    
+    <h5 class="text-center">MainFeatures</h5>
+    <h6>ChipSet: ${details.mainFeatures.chipSet}</h6>
+    <h6>DisplaySize:${details.mainFeatures.displaySize}</h6>
+    <h6>Memory: ${details.mainFeatures.memory}</h6>
+    <h5 class="text-center">Others</h5>
+    <h6>Bluetooth: ${details.others.Bluetooth}</h6>
+    <h6>GPS:${details.others.GPS}</h6>
+    <h6>NFC: ${details.others.NFC}</h6>
+    <h6>Radio:${details.others.Radio}</h6>
+    <h6>USB: ${details.others.USB}</h6>
+    <h6>WLAN: ${details.others.WLAN}</h6>
+    
+    
+   
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+
+    
+    `
+    
+
+
 }
