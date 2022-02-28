3aee85e893c9c90856368214f33867e589f49b80
Asadsheblu <shebluasad@gmail.com>
2022-02-28 23:22:11 +0600

    error-jandel-some

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const phoneSearch=()=>{
     //search value access
    const search=document.getElementById('search-box')
   const searchText=search.value ;
+  search.value=''
   const errorMsg= document.getElementById('errorMsg')
   errorMsg.innerText=''
 //error handeling
@@ -22,34 +23,40 @@ else{
 //function for display data
 
 const displayData=(datas)=>{
-    // console.log(datas.brand);
+    // console.log(datas);
 
     const searchResult=document.getElementById('search-result')
-    // const errorMsg= document.getElementById('errorMsg')
+    searchResult.textContent=''
+    //error handeling for wrong value
+    const errorMsg= document.getElementById('errorMsg')
+
+  errorMsg.innerText=''
+    if(datas.length==0){
     
-    //error handling for wrong search
-    // if(datas==null){
-    //     alert('error')
-    // }
-    
-    datas.forEach(data=>{
-        // console.log(data.phone_name);
-        const div=document.createElement('div')
-        div.classList.add('col')
-        div.innerHTML=`
-        <div class="card m-3">
-        <img  class='img-fluid w-75' src="${data.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h4 class="card-title">${data.phone_name}</h4>
-          <h5 class="card-title">${data.brand}</h5>
-          <button class="btn btn-success" onclick="details('${data.slug}')">Details</button>
-          
+      errorMsg.innerHTML="Oops!Your Search Result isn't  found"
+      return
+    }
+    else{
+        datas.forEach(data=>{
+            // console.log(data.phone_name);
+            const div=document.createElement('div')
+            div.classList.add('col')
+            div.innerHTML=`
+            <div class="card m-3">
+            <img  class='img-fluid w-75' src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h4 class="card-title">${data.phone_name}</h4>
+              <h5 class="card-title">${data.brand}</h5>
+              <button class="btn btn-success" onclick="details('${data.slug}')">Details</button>
+              
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-        `
-        searchResult.appendChild(div)
-    })
+            `
+            searchResult.appendChild(div)
+        })
+    }
+    
    
 }
 
@@ -61,13 +68,29 @@ const details=(id)=>{
 
 const displayDetails=(details)=>{
 // console.log(details);
+
     const phoneDetails=document.getElementById('details')
+    if(details!=0 ){
+        phoneDetails.innerHTML=`<h1 class='text-center fw-bold shadow=lg'>This Product Details is not available</h1>`
+    }
+    
     phoneDetails.innerHTML=`
+    
+    
     <div class="col-md-12 ">
     <img class="img-fluid" src="${details.image}" alt="" srcset="">
   <div class="card-body">
     <h5 class="card-title">${details.name}</h5>
-    <h6>${details.releaseDate}</h6>
+    
+       
+            if (details.releaseDate!=0) {
+                console.log('realsedate nai');
+            }
+        else{
+
+        }
+    
+   
     
     <h5 class="text-center">MainFeatures</h5>
     <h6>ChipSet: ${details.mainFeatures.chipSet}</h6>
